42bdbdf579492be0a710c7b0d9633ed42a7a41ca
Jeet Gajjar <94225096+JeetCoD@users.noreply.github.com>
2024-05-01 14:32:14 +0530

    Update supabase.js Edited supabase key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://gidgkyitbehdkovwrsow.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZGdreWl0YmVoZGtvdndyc293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5NDc3MjYsImV4cCI6MjAyNDUyMzcyNn0.z88RyPi4civdsZTwoVk6OvRXao4UBk-yXZrFyURK6a0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBmFzZSIsInJlZiI6ImdpZGdreWl0YmVoZGtvdndyc293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5NDc3MjYsImV4cCI6MjAyNDUyMzcyNn0.z88RyPi4civdsZTwoVk6OvRXao4UBk-yXZrFyURK6a0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
